1373474dd627cb1c3eaee80c2e6a66a478875feb
roshan <roshan.sharma@cogoport.com>
2025-02-17 12:06:21 +0530

    commented code deletion

--- src/main/java/com/snakeladder/GameController.java
+++ src/main/java/com/snakeladder/GameController.java
@@ -22,23 +22,6 @@ class GameController {
         board.initializeBoard(snakesAndLadders);
     }
 
-//    public void playGame() {
-//        boolean gameOver = false;
-//        while (!gameOver) {
-//            for (Player player : players) {
-//                int roll = dice.roll();
-//                System.out.println(player.getName() + " rolled a " + roll);
-//                player.move(roll, board);
-//                System.out.println(player.getName() + " moved to " + player.getPosition());
-//
-//                if (player.getPosition() == board.getSize()) {
-//                    System.out.println(player.getName() + " wins the game!");
-//                    gameOver = true;
-//                    break;
-//                }
-//            }
-//        }
-//    }
 public void playGame() {
     boolean gameOver = false;
     while (!gameOver) {
